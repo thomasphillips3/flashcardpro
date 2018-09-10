b61eb187d7efdda42cd01a6f481182ea74801e3e
Thomas Phillips <thomasphillips3@gmail.com>
2018-09-09 19:28:06 -0700

    Wrote tests for Stack component

---
+++ src/components/Stack.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Stack } from './Stack';
+import { stack } from '../data/fixtures';
+
+configure({ adapter: new Adapter() });
+
+const props = { stack };
+
+describe('Stack', () => {
+    const stack = shallow(<Stack {...props} />);
+
+    it('renders the title', () => {
+        expect(stack.find('h3').text()).toEqual(props.stack.title);
+    });
+
+    it('renders the link home', () => {
+        expect(stack.find('Link h4').text()).toEqual('Home');
+    });
+    
+    it('renders the right number of cards', () => {
+        expect(stack.find('Card').length).toEqual(props.stack.cards.length);
+    });
+})
---
+++ src/data/fixtures.js
@@ -0,0 +1,8 @@
+export const stack = {
+    id: 0,
+    title: "tests title",
+    cards: [
+        { id: 0, prompt: "butt prompt", answer: "test butt" },
+        { id: 1, prompt: "test butt", answer: "butt answer" }
+    ]
+};
