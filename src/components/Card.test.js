b6d8a365b328c4bec3dcde24a2531e6c163fe707
Thomas Phillips <thomasphillips3@gmail.com>
2018-11-06 17:49:39 -0800

    Wrote tests for Card component

---
+++ src/components/Card.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Card } from './Card';
+
+configure({
+    adapter: new Adapter()
+});
+
+const testPrompt = 'booty prompt';
+const testAnswer = 'booty answer';
+
+const card = { 
+    id: 0,
+    prompt: testPrompt,
+    answer: testAnswer
+}
+
+const props = { card }
+
+describe('Card', () => {
+    const card = shallow(<Card {...props} />)
+
+    it('renders the prompt without showing the answer', () => {
+        expect(card.find('h4').at(0).text()).toEqual(testPrompt);
+        expect(card.find('h4').at(1).hasClass('text-hidden')).toEqual(true);
+    });
+
+    describe('clicking the card', () => {
+        beforeEach(() => {
+            card.simulate('click');
+        });
+
+        it('reveals the answer', () => {
+            expect(card.find('h4').at(1).text()).toEqual(testAnswer);
+            expect(card.find('h4').at(1).hasClass('text-revealed')).toEqual(true);
+        });
+    });
+});
